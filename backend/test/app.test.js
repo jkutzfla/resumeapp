de70f9ae6c4542c64cd5255ac06a92192a5639e8
Josh Kutz-Flamenbaum <jkutzflamenbaum@gmail.com>
2025-03-03 09:26:07 -0500

    Basic Backend HTTP tests

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,6 +1,42 @@
 const assert = require('assert');
+const request = require('supertest');
+const express = require('express');
+const app = require('../app'); // Adjust the path as necessary
 describe('Backend Application', () => {
     it('hello world!', () => {
         assert.strictEqual(1 + 1, 2);
+    });
+});
+describe('GET /', () => {
+    it('should return 302 Found', (done) => {
+        request(app)
+            .get('/')
+            .expect(302, done);
+    });
+});
+
+describe('GET /api/test', () => {
+    it('should return 200 OK and contain check result', (done) => {
+        request(app)
+            .get('/api/test')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                assert(res.text.includes('Test OK, Time is:'));
+                done();
+            });
+    });
+});
+
+describe('Static html file', () => {
+    it('should return 200 OK and contain "Hello World"', (done) => {
+        request(app)
+            .get('/static.html')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                assert(res.text.includes('static html test'));
+                done();
+            });
     });
 });
